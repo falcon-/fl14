36a93a17f20e48cdd311dba66c948b1b0601c67e
falcon- <rafomalave@gmail.com>
2014-11-19 16:50:06 -0800

    Revert "Revert "Initial commit""
    This reverts commit 68c240ffcaa1c0e62a0059c4e2742dcb5cac6efc.

---
+++ hawaii.php
@@ -0,0 +1,8 @@
+<?php require 'include/config.php';?>
+<?php $title = "My Hawaii Page";?>
+<?php include 'include/header.php';?>
+<h1>My Hawaii Page</h1>
+<p>
+Clever content about Hawaii goes here!
+</p>
+<?php include 'include/footer.php';?>
